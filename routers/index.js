a66d727edf321f4f2e91570fdf351e3312a951f7
sinazay123 <sinazay123@gmail.com>
2022-03-18 17:32:01 -0500

    Added error handler for all routes using the errors object of Celebrate

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { errors } = require('celebrate');
 
 const router = express.Router();
 
 router.use(require('./UserRouter'));
+
+router.use(errors({ statusCode: 400, message: 'Validation failed.' }));
 
 module.exports = router;
