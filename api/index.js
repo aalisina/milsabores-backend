510d0f6c75984f6cf8c6938df1cc8f6b9cd5850c
sinazay123 <sinazay123@gmail.com>
2022-03-18 11:36:35 -0500

    Changed the name of the folder router to routers

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ extended: true }));
-api.use(require('../router'));
+api.use(require('../routers'));
 
 api.get('/', (req, res) => {
   res.send('Backend running.');
