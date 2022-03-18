3136ad0cf1de9b7b06d8274d6a438ec04c32cf8e
sinazay123 <sinazay123@gmail.com>
2022-03-18 11:52:19 -0500

    Added an await to get the response back

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 module.exports = {
   create: async (req, res) => {
     try {
-      const user = new User(req.body).save();
+      const user = await new User(req.body).save();
       res.status(201).json(user);
     } catch (err) {
       res.status(400).json(err);
