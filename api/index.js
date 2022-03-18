9e27cf9179ca439ea721222e716b632a82fd25d9
sinazay123 <sinazay123@gmail.com>
2022-03-18 10:58:57 -0500

    Added endpoint to create users

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models');
 
 const api = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,15 @@ api.use(express.json({ extended: true }));
 
 api.get('/', (req, res) => {
   res.send('Backend running.');
+});
+
+api.post('/api/users', async (req, res) => {
+  try {
+    const user = await new User(req.body).save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = { api, PORT };
