41da3741f4a78d01625efad79926a3b8dc2fc209
sinazay123 <sinazay123@gmail.com>
2022-03-18 17:39:45 -0500

    Added morgan middleware for loggin purpose

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const api = express();
 const PORT = process.env.PORT || 3000;
 
+api.use(morgan('common'));
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ extended: true }));
 api.use(require('../routers'));
