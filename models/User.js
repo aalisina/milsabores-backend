7413d630dd9f8e0f21e597873b5ec9c26cb1ebbb
sinazay123 <sinazay123@gmail.com>
2022-03-18 17:57:19 -0500

    Added password hashing functionality on the user scheme to save hashed passwords automatically

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_FACTOR = process.env.SALT_FACTOR || 10;
 
 const { Schema } = mongoose;
 
@@ -31,6 +34,27 @@ const userSchema = new Schema({
 }, {
   timestamps: true,
   versionKey: false,
+});
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  // only hash the password if it is being modified or is new
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  return bcrypt.genSalt(SALT_FACTOR, (errSalt, salt) => {
+    if (errSalt) return next(errSalt);
+
+    // hash the password
+    return bcrypt.hash(user.password, salt, (errHash, hash) => {
+      if (errHash) return next(errHash);
+
+      // override the normal text password with the hashed password
+      user.password = hash;
+      return next();
+    });
+  });
 });
 
 const User = mongoose.model('User', userSchema, 'Users');
