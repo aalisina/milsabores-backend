89bb2afeea8eca37f64d0835266bfa2988ecba1f
sinazay123 <sinazay123@gmail.com>
2022-03-17 21:59:55 -0500

    Added connection to DB

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* eslint-disable no-console */
+require('dotenv').config();
+const mongoose = require('mongoose');
 const { api, PORT } = require('./api');
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+)
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => console.log('Error occured while trying to connect to DB', err));
 
 api.listen(PORT, () => console.log(`Listening on ${PORT}`));
