1e1f9b41a7887735e3f1592c79a0d9b5a98bf6cf
sinazay123 <sinazay123@gmail.com>
2022-03-18 18:16:07 -0500

    removed bug because values are stored as strings in dotenv

--- models/User.js
+++ models/User.js
@@ -1,7 +1,10 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const SALT_FACTOR = process.env.SALT_FACTOR || 10;
+// eslint-disable-next-line radix
+const SALT_WORK_FACTOR = parseInt(process.env.SALT_FACTOR) || 10;
 
 const { Schema } = mongoose;
 
@@ -39,18 +42,18 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   const user = this;
 
-  // only hash the password if it is being modified or is new
+  // only hash the password if it has been modified or is new
   if (!user.isModified('password')) return next();
 
   // generate a salt
-  return bcrypt.genSalt(SALT_FACTOR, (errSalt, salt) => {
+  return bcrypt.genSalt(SALT_WORK_FACTOR, function (errSalt, salt) {
     if (errSalt) return next(errSalt);
 
-    // hash the password
-    return bcrypt.hash(user.password, salt, (errHash, hash) => {
+    // hash the password using our new salt
+    return bcrypt.hash(user.password, salt, function (errHash, hash) {
       if (errHash) return next(errHash);
 
-      // override the normal text password with the hashed password
+      // override the cleartext password with the hashed one
       user.password = hash;
       return next();
     });
