3c1049b48f96b19e79fa466b06a5a4d358563a25
sinazay123 <sinazay123@gmail.com>
2022-03-26 22:19:08 -0500

    Added configuration of environment variables for TEST, PRODUCTION and STAGING

---
+++ config/index.js
@@ -0,0 +1,17 @@
+const NODE_ENV = process.env.NODE_ENV || 'STAGING';
+
+const config = {
+  PRODUCTION: {
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vlobk.mongodb.net/production?retryWrites=true&w=majority`,
+  },
+  STAGING: {
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vlobk.mongodb.net/staging?retryWrites=true&w=majority`,
+  },
+  TEST: {
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vlobk.mongodb.net/test?retryWrites=true&w=majority`,
+  },
+};
+
+// eslint-disable-next-line no-console
+console.log('NODE_ENV', NODE_ENV);
+module.exports = config[NODE_ENV];
