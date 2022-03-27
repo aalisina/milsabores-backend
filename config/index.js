8f8bdde8b0aba2eca88187b882bc4991e7625a04
sinazay123 <sinazay123@gmail.com>
2022-03-26 22:24:34 -0500

    Changed text of what will be console.logged about env

--- config/index.js
+++ config/index.js
@@ -13,5 +13,5 @@ const config = {
 };
 
 // eslint-disable-next-line no-console
-console.log('NODE_ENV', NODE_ENV);
+console.log('Current node environment:', NODE_ENV);
 module.exports = config[NODE_ENV];
