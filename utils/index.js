ae21e393093541858ca77816711fb94361bcf6f8
sinazay123 <sinazay123@gmail.com>
2022-03-26 22:00:17 -0500

    Added role and email_verified to the payload of the token

--- utils/index.js
+++ utils/index.js
@@ -10,6 +10,8 @@ module.exports = {
       email: user.email,
       first_name: user.first_name,
       last_name: user.last_name,
+      role: user.role,
+      email_verified: user.email_verified,
       address: user.address,
       exp: Math.floor(Date.now() / 1000) + (60 * 60),
     };
