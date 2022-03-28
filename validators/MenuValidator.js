4f53aed2d0411c2b6500548ed8c42d9e5c9b97d9
sinazay123 <sinazay123@gmail.com>
2022-03-28 18:41:23 -0500

    Removed typo

--- validators/MenuValidator.js
+++ validators/MenuValidator.js
@@ -5,7 +5,7 @@ module.exports = {
     [Segments.BODY]: Joi.object().keys({
       protein: Joi.array().required(),
       drink: Joi.array().required(),
-      soup_fruit: Joi.array.required(),
+      soup_fruit: Joi.array().required(),
       sides: Joi.array().required(),
     }),
   }),
