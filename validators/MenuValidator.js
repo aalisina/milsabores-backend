78a1dcc3471c9198f650c3f2d3ac50a02f61a0a1
sinazay123 <sinazay123@gmail.com>
2022-03-28 18:40:19 -0500

    Added validator for creating a menu

---
+++ validators/MenuValidator.js
@@ -0,0 +1,12 @@
+const { celebrate, Joi, Segments } = require('celebrate');
+
+module.exports = {
+  create: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      protein: Joi.array().required(),
+      drink: Joi.array().required(),
+      soup_fruit: Joi.array.required(),
+      sides: Joi.array().required(),
+    }),
+  }),
+};
--- validators/index.js
+++ validators/index.js
@@ -1,7 +1,9 @@
 const UserValidator = require('./UserValidator');
 const OrderValidator = require('./OrderValidator');
+const MenuValidator = require('./MenuValidator');
 
 module.exports = {
   UserValidator,
   OrderValidator,
+  MenuValidator,
 };
