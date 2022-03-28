f3184b41826f87a66eb04fd5579bd4f3358540cc
sinazay123 <sinazay123@gmail.com>
2022-03-28 14:25:10 -0500

    Tested whether the verifyTokenAdmin method was working properly

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const { UserValidator } = require('../validators');
 const { UserController } = require('../controllers');
-const { verifyTokenUser } = require('../middlewares');
+const { verifyTokenUser, verifyTokenAdmin } = require('../middlewares');
 
 router.post('/users', verifyTokenUser, UserValidator.create, UserController.create);
-router.get('/users', UserController.findAll);
+router.get('/users', verifyTokenAdmin, UserController.findAll);
 router.get('/users/:id', UserValidator.findOne, UserController.findOne);
 router.patch('/users/:id', UserValidator.updateOne, UserController.updateOne);
 router.delete('/users/:id', UserValidator.deleteOne, UserController.deleteOne);
