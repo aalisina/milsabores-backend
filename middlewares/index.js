f2d6882b05ba13001fd7e25c18408fd1de198ac2
sinazay123 <sinazay123@gmail.com>
2022-03-28 14:24:26 -0500

    Added a middleware to check whether a user is ADMIN

--- middlewares/index.js
+++ middlewares/index.js
@@ -14,4 +14,22 @@ module.exports = {
       res.status(401).json({ message: 'Auth error' });
     }
   },
+  verifyTokenAdmin: async (req, res, next) => {
+    try {
+      const { authorization } = req.headers;
+      // Bearer eyJhbGciOiJIUz9.eyJpZCI6IjVmMDN.jNTFkZWUw
+      // authorization.split(' '); // -> [ "Bearer", "eyJhbG..." ]
+      const token = authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const { role } = decoded;
+      if (role === 'ADMIN') {
+        req.decoded = decoded;
+        next();
+      } else {
+        res.status(400).json({ message: 'Not authorized as admin' });
+      }
+    } catch (err) {
+      res.status(401).json({ message: 'Auth error' });
+    }
+  },
 };
