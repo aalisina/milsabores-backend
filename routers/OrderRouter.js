65f2d8509cc25764ccc63a947902d399d6aeec9d
sinazay123 <sinazay123@gmail.com>
2022-04-01 11:24:41 -0500

    Added route to for getting all the orders of today

--- routers/OrderRouter.js
+++ routers/OrderRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { OrderValidator } = require('../validators');
 const { OrdersController } = require('../controllers');
+const { verifyTokenAdmin } = require('../middlewares');
 
 const router = express.Router();
 
 router.post('/orders', OrderValidator.create, OrdersController.create);
 router.get('/orders', OrdersController.findAll);
 router.get('/orders/:userId', OrdersController.findOrdersUser);
+router.get('/orders/today', verifyTokenAdmin, OrdersController.findOrdersToday);
 
 module.exports = router;
