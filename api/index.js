3e7f3de203541b0e0374e2357a1a3b2ecf1e5187
sinazay123 <sinazay123@gmail.com>
2022-04-04 12:26:32 -0500

    Imported ejs, connect-flash, path and configured the view engine in the api module

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const session = require('express-session');
 const morgan = require('morgan');
 const cors = require('cors');
+const flash = require('connect-flash');
+const path = require('path');
 
 const api = express();
+
+// Setting up view engine
+api.set('views', path.join(__dirname, 'views'));
+api.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;
 
+api.use(session({
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  saveUninitialized: true,
+  resave: true,
+}));
+api.use(flash());
 api.use(cors());
 api.use(morgan('common'));
 api.use(express.urlencoded({ extended: true }));
