54d474deb7ced5c41e40781c8b5a0ac8c33a4375
sinazay123 <sinazay123@gmail.com>
2022-04-04 15:27:22 -0500

    Removed no longer needed dependencies for this project

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const morgan = require('morgan');
 const cors = require('cors');
-const flash = require('connect-flash');
+// const flash = require('connect-flash');
 const path = require('path');
 
 const api = express();
@@ -12,12 +12,12 @@ api.set('views', path.join(__dirname, '../views'));
 api.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;
 
-api.use(session({
-  secret: process.env.EXPRESS_SESSION_SECRET,
-  saveUninitialized: true,
-  resave: true,
-}));
-api.use(flash());
+// api.use(session({
+//   secret: process.env.EXPRESS_SESSION_SECRET,
+//   saveUninitialized: true,
+//   resave: true,
+// }));
+// api.use(flash());
 api.use(cors());
 api.use(morgan('common'));
 api.use(express.urlencoded({ extended: true }));
