577243a91f7ba345c80d75ce48a0559dbf1f14d1
sinazay123 <sinazay123@gmail.com>
2022-04-06 11:50:56 -0500

    Setting form action to the url string

--- public/assets/change-pass/index.js
+++ public/assets/change-pass/index.js
@@ -1,4 +1,4 @@
-const url_string = window.location.href;
+let url_string = window.location.href;
 const params = url_string.split('/forgot/')[1];
 const userId = params.split('/')[0];
 const key = params.split('/')[1];
@@ -6,3 +6,5 @@ const baseUrlBack = url_string.split('/api/')[0]
 // console.log('userId is :', userId);
 // console.log('Key is :', key);
 // console.log('Base url is :', baseUrlBack);
+const form = document.getElementById('form')
+form.action = url_string;
