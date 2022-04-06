dca529b86bda7b58fae7089c1a85fe184180a3d6
sinazay123 <sinazay123@gmail.com>
2022-04-05 20:59:49 -0500

    Only admins can post a menu

--- routers/MenuRouter.js
+++ routers/MenuRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { MenuValidator } = require('../validators');
 const { MenuController } = require('../controllers');
+const { verifyTokenAdmin } = require('../middlewares');
 
 const router = express.Router();
 
-router.post('/menu', MenuValidator.create, MenuController.create);
+router.post('/menu', verifyTokenAdmin, MenuValidator.create, MenuController.create);
 router.get('/menu', MenuController.getMenu);
 
 module.exports = router;
