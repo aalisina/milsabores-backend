c034736a987df2e99daaad56a3e4649b3d937ae0
sinazay123 <sinazay123@gmail.com>
2022-04-05 21:02:40 -0500

    Protected some user routes from regular users

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -5,10 +5,10 @@ const { UserValidator } = require('../validators');
 const { UserController } = require('../controllers');
 const { verifyTokenUser, verifyTokenAdmin } = require('../middlewares');
 
-router.post('/users', verifyTokenUser, UserValidator.create, UserController.create);
+router.post('/users', verifyTokenAdmin, UserValidator.create, UserController.create);
 router.get('/users', verifyTokenAdmin, UserController.findAll);
-router.get('/users/:id', UserValidator.findOne, UserController.findOne);
-router.patch('/users/:id', UserValidator.updateOne, UserController.updateOne);
-router.delete('/users/:id', UserValidator.deleteOne, UserController.deleteOne);
+router.get('/users/:id', verifyTokenUser, UserValidator.findOne, UserController.findOne);
+router.patch('/users/:id', verifyTokenAdmin, UserValidator.updateOne, UserController.updateOne);
+router.delete('/users/:id', verifyTokenAdmin, UserValidator.deleteOne, UserController.deleteOne);
 
 module.exports = router;
