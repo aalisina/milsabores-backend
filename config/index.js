07244fe2662be6366e7c967ae1708a5bc6291abd
sinazay123 <sinazay123@gmail.com>
2022-04-07 21:35:46 -0500

    Added another env variable to be able to connect to different clusters

--- config/index.js
+++ config/index.js
@@ -2,16 +2,15 @@ const NODE_ENV = process.env.NODE_ENV || 'STAGING';
 
 const config = {
   PRODUCTION: {
-    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vlobk.mongodb.net/production?retryWrites=true&w=majority`,
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/production?retryWrites=true&w=majority`,
   },
   STAGING: {
-    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vlobk.mongodb.net/staging?retryWrites=true&w=majority`,
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/staging?retryWrites=true&w=majority`,
   },
   TEST: {
-    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vlobk.mongodb.net/test?retryWrites=true&w=majority`,
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`,
   },
 };
 
-// eslint-disable-next-line no-console
 console.log('Current node environment:', NODE_ENV);
 module.exports = config[NODE_ENV];
