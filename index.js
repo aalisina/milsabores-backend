21557e0371066fbc24a35e8f69eff6972895b9f2
sinazay123 <sinazay123@gmail.com>
2022-05-09 11:37:53 -0500

    Commented out unused function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { Server } = require('socket.io');
 const http = require('http');
 const { api, PORT } = require('./api');
-const { verifySocketAdmin } = require('./middlewares');
+// const { verifySocketAdmin } = require('./middlewares');
 
 api.use(express.static('public'));
 const server = http.createServer(api);
