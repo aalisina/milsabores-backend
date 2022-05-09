c617fd29547e331c45486f5638054641fe03f87e
sinazay123 <sinazay123@gmail.com>
2022-05-09 16:00:28 -0500

    Added a long time for the token to last

--- utils/index.js
+++ utils/index.js
@@ -14,7 +14,7 @@ module.exports = {
       role: user.role,
       email_verified: user.email_verified,
       address: user.address,
-      exp: Math.floor(Date.now() / 1000) + (60 * 60),
+      exp: '10000d',
     };
     try {
       const token = jwt.sign(payload, process.env.JWT_SECRET);
